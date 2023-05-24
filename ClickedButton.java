9b5dee78f072bfc3e3dbeb0ee9fefee554514e49
victorsimonp <132145995+victorsimonp@users.noreply.github.com>
2023-05-24 12:00:32 -0300

    Add files via upload

---
+++ ClickedButton.java
@@ -0,0 +1,42 @@
+public class ClickedButton {
+    private Carta carta1;
+    private Carta carta2;
+
+    public void setCarta1(Carta carta1) {
+        this.carta1 = carta1;
+    }
+
+    public void setCarta2(Carta carta2) {
+        this.carta2 = carta2;
+    }
+
+    public Carta getCarta1() {
+        return carta1;
+    }
+
+    public Carta getCarta2() {
+        return carta2;
+    }
+
+    public boolean verificarPar() {
+        if (carta1 != null && carta2 != null) {
+            if (carta1.getResposta().equals("for") && carta2.getResposta().equals("for")) {
+                // Par correto com a resposta "for"
+                return true;
+            } else {
+                // Par incorreto
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void desvirarCartas() {
+        if (carta1 != null && carta2 != null) {
+            carta1.virarCarta();
+            carta2.virarCarta();
+            carta1 = null;
+            carta2 = null;
+        }
+    }
+}
